6d722e293b6772702ba0f2ebd500db079a174a05
nicsep <nsepulveda92@gmail.com>
2020-06-16 15:32:39 -0400

    added pokecards and sprites

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
+import "./App.css"
+import PokeCard from './components/PokeCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemons : [],
+      pokemonDetails : [],
+    }    
+  }
+
+  componentDidMount() {
+    this.getMorePokemon();
+  }
+
+  getMorePokemon() {
+    let url = "https://pokeapi.co/api/v2/pokemon?offset=" + this.state.offset + "&limit=" + this.state.loadNumber;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data) {
+        this.setState({pokemons : data.results}, () => {
+          this.state.pokemons.map(pokemon => {
+            fetch(pokemon.url)
+            .then(response => response.json())
+            .then(data => {
+              if (data) {
+                var temp = this.state.pokemonDetails
+                temp.push(data)
+                this.setState({pokemonDetails: temp})
+              }            
+            })
+            .catch(console.log)
+          })
+        })        
+      }
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    const {pokemonDetails} = this.state;
+
+    const renderedPokemonList = pokemonDetails.map((pokemon, index) => {
+      return (<PokeCard pokemon={pokemon} />);
+    });
+
+    return (
+      <div className="container">
+        <div className="card-columns">
+          {renderedPokemonList}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
